896add51a2624f79ffd46915849013102f01017d
bdowds <bwdowds@gmail.com>
2018-09-12 19:23:36 -0500

    Initial Commit

---
+++ SQLIntro/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SQLIntro
+{
+    class ProductRepository
+    {
+        private static string connectionString = "Server=localhost;Database=adventureworks;Uid=root;Pwd=password;";
+
+        public List<Product> GetProducts()
+        {
+
+
+
+            return new List<Product>();
+        }
+    }
+}
--- SQLIntro/Program.cs
+++ SQLIntro/Program.cs
@@ -6,7 +6,7 @@ namespace SQLIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
